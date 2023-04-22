a26e2e1099f037b948c8702c9ad02fa494b301ad
Anton <akira78nl@gmail.com>
2023-04-22 13:52:44 +0500

    initial commit

---
+++ Homework/lesson1/Program.cs
@@ -0,0 +1,7 @@
+﻿// Задача 1.
+Console.WriteLine("Введите первое число :");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число :");
+int num2 = Convert.ToInt32(Console.ReadLine());
+if(num1 > num2) Console.WriteLine("Первое число больше второго");   
+else Console.WriteLine("Второе число больше первого"); 
---
+++ Seminars/Seminar1/Program.cs
@@ -0,0 +1,40 @@
+﻿/*Console.WriteLine("Введи цифру, сучара !");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Ты ввёл {num} цифру");*/
+
+//Task1. Запросить число и вывести его квадрат
+/*Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Квадрат числа - {(num*num)}");*/
+
+//Task2. Является ли первое число квадратом второго числа ?
+/*Console.WriteLine("Введите 1 число : ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите 2 число : ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+if((num2*num2) == num1)
+{
+  Console.WriteLine("Первое число - это квадрат второго.");  
+}
+else{Console.WriteLine("Первое число - не квадрат второго !");}*/
+
+//Task3. Напищите программу которая принимает на вход число N, на выходе получаем последовательность от -N до N
+/*Console.WriteLine("Введите N : ");
+int n = Convert.ToInt32(Console.ReadLine());
+if(n<0){Console.WriteLine("Invalid input N");}
+int count = n*(-1);
+while(count <= n)
+{
+    Console.WriteLine(count+" ");
+    count++;
+}*/
+
+//Task 4. На вход принимаем 3х значное число, вывести последнюю цифру данного числа.
+/*
+Console.WriteLine("Введите трёхзначное число");
+int n = Convert.ToInt32(Console.ReadLine());
+int ed = n % 10;
+Console.WriteLine($"Последняя цифра числа - {ed}");*/
+int a = 1;
+Console.WriteLine(a.GetType());
+Console.ReadLine();
