af9342498615b391c2da230c40f55810c736f4b1
Anton <akira78nl@gmail.com>
2023-04-22 14:09:57 +0500

    Vtorauya_zadacha

--- Homework/lesson1/Program.cs
+++ Homework/lesson1/Program.cs
@@ -1,7 +1,27 @@
 ﻿// Задача 1.
+/*
 Console.WriteLine("Введите первое число :");
 int num1 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите второе число :");
 int num2 = Convert.ToInt32(Console.ReadLine());
-if(num1 > num2) Console.WriteLine("Первое число больше второго");   
-else Console.WriteLine("Второе число больше первого"); 
+if(num1 > num2) Console.WriteLine($"Первое число - {num1} больше второго - {num2}");   
+else Console.WriteLine("Второе число больше первого");*/
+
+// Задача 2.
+/*
+Console.WriteLine("Введите первое число :");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число :");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число :");
+int num3 = Convert.ToInt32(Console.ReadLine());
+if(num1>num2)
+{
+    if (num1 > num3) {Console.WriteLine($"Максимальное - первое введённое число  - {num1}");}
+}
+else
+{
+    if (num2 > num3){Console.WriteLine($"Максимальное - второе введённое число  - {num2}");}
+    else {Console.WriteLine($"Максимальное - третье введённое число  - {num3}");}
+}
+*/
