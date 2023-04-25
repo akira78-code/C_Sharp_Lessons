a23b6488246bb8bfb9b36a18b1c95d76bc37b11e
Anton <akira78nl@gmail.com>
2023-04-25 11:58:52 +0500

    homework2

--- Homework/lesson2/Program.cs
+++ Homework/lesson2/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//1 Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+/*
+void TwoDigit (int num)
+{
+    int result = (num % 100) / 10;
+    Console.Write($"Второе число -> {result}");
+}
+TwoDigit(164);
+*/
