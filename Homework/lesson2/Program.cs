78be48d776a08531c94daf46dbaa7320036d14e5
Anton <akira78nl@gmail.com>
2023-04-25 13:30:53 +0500

    2_lessons_2_zadacha

--- Homework/lesson2/Program.cs
+++ Homework/lesson2/Program.cs
@@ -7,3 +7,24 @@ void TwoDigit (int num)
 }
 TwoDigit(164);
 */
+
+//2  Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+void ThreeDigit (int num)
+{
+    // Проверяем что число не трёхзначное. т.к любое не трёхзначное при делении на 100 в int даст 0
+    if (num / 100 == 0) {Console.WriteLine("Число не трёхзначное !");} 
+    else {
+    int num_m = num;
+    //
+    int exp_num = 1;
+    while(num_m != 0)
+{
+num_m = num_m / 10;
+exp_num = exp_num * 10;
+}
+exp_num = exp_num / 100;
+int result = (num % exp_num) / (exp_num / 10);
+Console.WriteLine($"Третья цифра числа -> {result}");
+}}
+ThreeDigit(478568795);
