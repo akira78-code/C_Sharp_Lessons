404700bc8e7fea256789ce1fad50e23503335da4
Anton <akira78nl@gmail.com>
2023-05-19 17:29:37 +0500

    homework4

---
+++ Homework/lesson4/Program.cs
@@ -0,0 +1,18 @@
+﻿//Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+/*
+int NaturStepen(int num, int stepen)
+{
+    int CopyNum = num;
+    int r = 0;
+   for(int count = 1; count<stepen;count++)
+   {
+        
+        r = CopyNum*num;
+        Console.WriteLine(CopyNum);
+        CopyNum = r;
+   }
+   return CopyNum;
+}
+Console.WriteLine($"{NaturStepen(2,3)} - результат");*/
+
+//Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
