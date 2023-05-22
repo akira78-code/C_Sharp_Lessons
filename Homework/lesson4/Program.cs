71b852ce9109b316d1a503c4fde2b39189f47166
Anton <akira78nl@gmail.com>
2023-05-22 09:25:56 +0500

    zadach_complete

--- Homework/lesson4/Program.cs
+++ Homework/lesson4/Program.cs
@@ -16,3 +16,35 @@ int NaturStepen(int num, int stepen)
 Console.WriteLine($"{NaturStepen(2,3)} - результат");*/
 
 //Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+/*
+
+int SumCifr(int num)
+{
+    int a = 10;
+    int b = 1;
+    int i = 1;
+    int result = 0;
+    while(i != 0)
+    {
+        i = (num%a)/b;
+        result+=i;
+        a*=10;
+        b*=10;
+    }
+    return result;
+}
+Console.WriteLine(SumCifr(1234));*/
+
+//Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+/*void CreateArroy()
+{
+   Console.WriteLine("Введите числа, через запятую :");
+   string nums =  Console.ReadLine();
+   string [] arroy = nums.Split(new char[] {','});
+   foreach(string i in arroy)
+   {
+    Console.WriteLine(i);
+   }
+}
+CreateArroy();*/
